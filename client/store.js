abcf81e9f0507b02f4dde8fd27db3737fcc3a1a4
kassanj <kassandram022@gmail.com>
2019-10-18 10:20:31 -0700

    configure thunk middleware

--- client/store.js
+++ client/store.js
@@ -1,4 +1,5 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import reducers from './reducers/index';
 import { loadState, saveState } from './localStorage';
@@ -8,14 +9,14 @@ const persistedState = loadState();
 const store = createStore(
   reducers,
   persistedState,
-  composeWithDevTools()
+  composeWithDevTools(applyMiddleware(thunk))
 );
 
 store.subscribe(() => {
-  // saveState({
-  //   trackList: store.getState().trackList,
-  //   user: store.getState().user
-  // });
+  saveState({
+    trackList: store.getState().trackList,
+    user: store.getState().user
+  });
 });
 
 export default store;
